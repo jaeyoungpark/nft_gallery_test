f82f498343b1bec1e819faf9979a06124d433f21
Michael Chun <771400+mikechun@users.noreply.github.com>
2022-01-26 16:06:18 -0800

    Use customized theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { ThemeProvider } from '@emotion/react';
+
 import { AuthProvider } from './context/AuthProvider';
 import { AwsProvider } from './context/AwsProvider';
-
+import { theme } from './theme.ts'
 import Main from './Main';
 
+console.log(theme);
 function App() {
   return (
     <AuthProvider>
       <AwsProvider>
-        <Main />
+        <ThemeProvider theme={theme}>
+          <Main />
+        </ThemeProvider>
       </AwsProvider>
     </AuthProvider>
   );
